f10117f0861e71a197a1d6a1163e22a5a63d67d7
Hoon Oh <shoh@help-me.kr>
2021-08-19 12:16:58 +0900

    chore: 실행환경 설정
    개발시 node_env 를 development로 실행하도록 설정 및 실행환경 별 알맞은 환경변수 파일을 가져와 활용하도록 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      validationSchema: Joi.object({
+        DB_TYPE: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
