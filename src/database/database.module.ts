fc6338f20254551d0ba27a74c4015ae52fc2d394
Hoon Oh <shoh@help-me.kr>
2021-08-20 10:52:13 +0900

    refactor: 데이터베이스 엔티티 로드
    폴더 내의  엔티티 등록 로직 추가

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -15,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         synchronize: false,
-        entities: [],
+        entities: [__dirname + '/../**/*.entity.ts'],
       }),
     }),
   ],
