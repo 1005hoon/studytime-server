a5bcc2e10d507d6189789e4275607ecb9e85e94a
Hoon Oh <shoh@help-me.kr>
2021-08-27 10:26:12 +0900

    chore: 앱 시작시 포트 로그

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, () => {
+    logger.log(`PORT: ${process.env.PORT}`);
+  });
 }
 bootstrap();
