2ec232835136904dbe83704e75a1b5afcd64299c
Hoon Oh <shoh@help-me.kr>
2021-08-28 02:52:42 +0900

    test

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -10,6 +10,14 @@ export class SlackController {
     console.log(body);
     console.log('--------------------------------');
     console.log(body.payload);
+    console.log('--------------------------------');
+    console.log('PAYLOAD TO OBJECT');
+    const payloadToObject = body.payload.toObject();
+    console.log(payloadToObject);
+    console.log('--------------------------------');
+    console.log('PAYLOAD TO JSON');
+    const payloadToJSON = body.payload.toJSON();
+    console.log(payloadToJSON);
 
     // const { name: actionName, value: actionPayload } = actions[0];
     // const userName = user.name;
