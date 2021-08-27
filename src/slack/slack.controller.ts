c83f916f5ccdd55b8e0bbc714190e2f85391d776
Hoon Oh <shoh@help-me.kr>
2021-08-27 23:22:52 +0900

    refactor: slack response test

--- src/slack/slack.controller.ts
+++ src/slack/slack.controller.ts
@@ -4,6 +4,8 @@ import { Body, Controller, Post } from '@nestjs/common';
 export class SlackController {
   @Post()
   test(@Body() body) {
+    console.log(body);
+    console.log(body.toJSON());
     return body;
   }
 }
