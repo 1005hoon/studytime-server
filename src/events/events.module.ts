a06fb49830a31e59dc3bd3b539c59b5c6f61c6b8
Hoon Oh <shoh@help-me.kr>
2021-12-27 16:27:56 +0900

    feat: wire events module with events controller for api handling

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import EventDetail from './entities/event-details.entity';
 import Event from './entities/events.entity';
+import { EventsController } from './events.controller';
 import { EventsService } from './events.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EventDetail, Event])],
+  controllers: [EventsController],
   providers: [EventsService],
 })
 export class EventsModule {}
