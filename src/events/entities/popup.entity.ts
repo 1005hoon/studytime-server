5ad2c0bc2cdd8180b53a951218fe3e258190d408
Hoon Oh <shoh@help-me.kr>
2022-01-10 17:14:36 +0900

    feat(popup): 팝업 엔티티 작성

---
+++ src/events/entities/popup.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('popup', { schema: 'studytime' })
+export class Popup {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column('varchar', { name: 'screen', nullable: true, length: 20 })
+  screen: string | null;
+
+  @Column('int', { name: 'target_id', nullable: true })
+  targetId: number | null;
+
+  @Column('varchar', { name: 'img_url', length: 256 })
+  imgUrl: string;
+
+  @Column('varchar', { name: 'url', nullable: true, length: 256 })
+  url: string | null;
+
+  @Column('tinyint', { name: 'use_yn', width: 1 })
+  useYn: boolean;
+
+  @Column('datetime', { name: 'created_at' })
+  createdAt: Date;
+
+  @Column('tinyint', { name: 'is_deleted', width: 1 })
+  isDeleted: boolean;
+
+  @Column('varchar', { name: 'description', length: 256 })
+  description: string;
+}
