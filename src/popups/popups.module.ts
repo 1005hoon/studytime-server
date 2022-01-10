438565123a4d192ad5cd167d82a2ae4d15833c29
Hoon Oh <shoh@help-me.kr>
2022-01-10 18:00:47 +0900

    feat(Popup): 팝업 구현에 필요한 리포지토리와 이미지 저장 목적의 파일서비스 모듈 의존성 주입

--- src/popups/popups.module.ts
+++ src/popups/popups.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FilesModule } from 'src/files/files.module';
 import { PopupsController } from './popups.controller';
+import { PopupsRepository } from './popups.repository';
 import { PopupsService } from './popups.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PopupsRepository]), FilesModule],
   controllers: [PopupsController],
-  providers: [PopupsService]
+  providers: [PopupsService],
 })
 export class PopupsModule {}
--- src/popups/popups.service.ts
+++ src/popups/popups.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FilesService } from 'src/files/files.service';
+import { PopupsRepository } from './popups.repository';
 
 @Injectable()
-export class PopupsService {}
+export class PopupsService {
+  constructor(
+    @InjectRepository(PopupsRepository)
+    private readonly popupsRepository: PopupsRepository,
+    private readonly filesService: FilesService,
+  ) {}
+}
